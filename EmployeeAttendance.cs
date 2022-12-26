9fd76b5e6c165dd74b7619427e47d27543819cc8
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-26 11:17:18 +0530

    Employee atteandence added

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProblem
 {
-    internal class EmployeeAttendance
+    public class EmployeeAttendance
     {
+        public static void CheckAttendance()
+        {
+            int IS_FULL_TIME = 1;
+
+            Random random= new Random();
+            int empCheck = random.Next(2);
+            if(empCheck==IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
     }
 }
