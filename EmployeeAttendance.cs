2fc59134fd5b6aec44a80cc174913b1eec6f3899
“Yashwanth142” <yashwanthvishnu14@gmail.com>
2022-12-27 10:20:51 +0530

    Part Time added

--- EmployeeAttendance.cs
+++ EmployeeAttendance.cs
@@ -12,22 +12,30 @@ namespace EmployeeWageProblem
         {
             int IS_FULL_TIME = 1;
             int EMP_RATE_PER_HOUR = 20;
+            int IS_PART_TIME = 2;
 
             int empHrs = 0;
             int empWage = 0;
 
             Random random= new Random();
-            int empCheck = random.Next(2);
+            int empCheck = random.Next(3);
+
             if(empCheck==IS_FULL_TIME)
             {
                 Console.WriteLine("Employee is Present");
                 empHrs = 8;
+            }
+            else if(empCheck==IS_PART_TIME)
+            {
+                Console.WriteLine("Employye is Part time");
+                empHrs = 4;
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
                 empHrs= 0;
             }
+
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("The Employee Wage is " + empWage);
         }
